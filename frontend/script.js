729fc378acd9377e1b1e22b910d802651549e7d9
shauri-123 <shaurirebecca23@gmail.com>
2026-05-30 22:03:51 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
-const API = 'https://rebecca-portfolio-api.onrender.com/';
-
+const API = 'https://rebecca-portfolio-api.onrender.com';
 function setText(id, value) {
   const el = document.getElementById(id);
   if (el) el.textContent = value;
